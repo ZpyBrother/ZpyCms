3dede31082a02bce84d8e7f7dda7959d06d5c769
ZpyBrother <295051185@qq.com>
2016-11-25 15:03:00 +0800

    提交

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ZpyCms
-基于61PHP开发的MVC结构的纯净版后台，仅有基础的登录以及权限管理等功能，方便二次开发
+##Zpy`s CMS
+
+基于61PHP+Layui开发的MVC结构的纯净版后台，仅有基础的登录以及权限管理等功能，方便二次开发
+
+##相关截图
+<img src='http://www.zhoupengyu.cn/images/cut.png'>
+##有问题反馈
+在使用中有任何问题，欢迎反馈给我，可以用以下联系方式跟我交流
+
+* 邮件(295051185@qq.com)
+* QQ: 295051185
+* Blog: [Zpy](http://www.zhoupengyu.cn)
+
+
+
+##感激
+* [核心框架支持 61PHP](https://git.oschina.net/61php/PHP-framework-61php)
+
+##关于作者
+
+无名的PHPer
---
+++ include/include.php
@@ -0,0 +1,14 @@
+<?php
+require( 'config/config.php');
+require( 'config/db.php');
+require( 'class/GlobalVariant.php');
+require( 'class/sqlToolClass.php');
+require( 'class/mySql.php');
+require( 'smarty/Smarty.class.php');
+require( 'lib/verify.php');
+require( 'lib/ConfigClass.php');
+require( 'lib/MessageClass.php');
+require( 'language/'._DEFAULT_LANGUAGE.".php");
+require( 'framework/coreFramework.php');
+if (_DATABASE_OPEN=="1") $db =  new MySqlClass();
+$GVar = new GlobalVariant();
