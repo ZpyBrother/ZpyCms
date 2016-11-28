50db75743493dda51b37b1ac35ef0f56c8ef3ae8
ZpyBrother <295051185@qq.com>
2016-11-28 12:48:07 +0800

    更新部分界面ui

--- include/config/config.php
+++ include/config/config.php
@@ -14,3 +14,18 @@ define("_SITE_CACHE",_SITE_ROOT.DS."cache".DS);
 define("_SITE_APPLICATION_PATH",_SITE_ROOT.DS."application".DS);
 define("_SITE_APPLICATION_TEMPLATE",_SITE_APPLICATION_PATH."template".DS._DEFAULT_TEMPLATE.DS._DEFAULT_LANGUAGE.DS);
 
+//数据库设定
+
+define('_DATABASE_HOST','localhost');
+
+define('_DATABASE_USER','root');
+
+define('_DATABASE_PASSWORD','123456');
+
+define('_DATABASE_NAME','php-cms');
+
+define('_DATABASE_UT','utf8');
+
+define('_TABLE_FIRST_NAME','pc');
+
+define('_DATABASE_OPEN','1'); #是否开启数据库连接  1开启 2关闭
